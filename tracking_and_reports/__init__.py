194626fb1625a3eea8548d8766dd62c3aac3b4aa
PranavYugan <yuganpranav@gmail.com>
2026-04-23 22:28:07 +0530

    Update(Pranav): Updated init file for tracking and reports

--- tracking_and_reports/__init__.py
+++ tracking_and_reports/__init__.py
@@ -1,4 +1,5 @@
 from .gambler_statistics import GamblerStatistics
 from .stake_history_report import StakeHistoryReport
+from .win_loss_statistics import WinLossStatistics
 
-__all__ = ["GamblerStatistics", "StakeHistoryReport"]
+__all__ = ["GamblerStatistics", "StakeHistoryReport", "WinLossStatistics"]
